325c7231c7e7dfd6ec4d5915604619fd7b003055
Yuxi Chen <yuxichen@umich.edu>
2022-09-19 16:52:42 -0400

    Added task11.1 - 11.4

---
+++ examples/MyVector.h
@@ -0,0 +1,11 @@
+// Step 1: add header guards. What are header guards? Why are they necessary?
+
+// Step 2: Create an empty template class MyVector. 
+
+// Step 3: Implement the following methods:
+// 1. void push_back(T data);
+// 2. void insert(int index, T data);
+// 3. T get (int index);
+// 4. int size();
+// 5. int capacity();
+// 6. a destructor to release memory. 
--- examples/ex.cpp
+++ examples/ex.cpp
@@ -1,13 +1,12 @@
 #include "../AMREX/InstallDir/include/AMReX.H"
 #include "../AMREX/InstallDir/include/AMRex_ParallelDescriptor.H"
-#include "../AMREX/InstallDir/include/AMRex_Particles.H"
 #include "../AMREX/InstallDir/include/AMRex_ParticleContainer.H"
+#include "../AMREX/InstallDir/include/AMRex_Particles.H"
 #include "../AMREX/InstallDir/include/AMRex_Vector.H"
 #include <iostream>
 
 #include "ex.h"
 using namespace amrex;
-
 
 int main(int argc, char* argv[]) {
   amrex::Initialize(argc, argv);
@@ -55,72 +54,77 @@ int main(int argc, char* argv[]) {
   // Task 9 [optional]: redo Task 8 with multiple processors.
 
   //=============== Particles =====================
-  // Task 10: Initialize a Particle object. Set particle initial velocity, location and ID. 
- Particle<1,1> pp1;
+  // Task 10: Initialize a Particle object. Set particle initial velocity,
+  // location and ID.
+  Particle<1, 1> pp1;
 
-  // Task 11: Do NOT use ParticleContainer. Creat a list of Particles with std::vector or std::array or std::list. What is the difference between these three containers? Which do you think is the best for our purpose? 
-  std::vector<amrex::Particle<1,1>> pv1;
-  Particle<1,1> p1;
-  Particle<1,1> p2;
+  // Task 11: Do NOT use ParticleContainer. Creat a list of Particles with
+  // std::vector or std::array or std::list. What is the difference between
+  // these three containers? Which do you think is the best for our purpose?
+  std::vector<amrex::Particle<1, 1> > pv1;
+  Particle<1, 1> p1;
+  Particle<1, 1> p2;
 
-  
   pv1.push_back(p1);
   pv1.push_back(p2);
 
-  //... I know what the difference is but cant say for sure which is better. Array wont work if particles are added or removed. List will be faster for adding or removing particles but might be slower when we iterate through the particles.
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //... I know what the difference is but cant say for sure which is better.
+  // Array wont work if particles are added or removed. List will be faster for
+  // adding or removing particles but might be slower when we iterate through
+  // the particles.
 
+  // Task 11.1: loop through and print location of all particles with the
+  // traditional loop: for(int i = 0......)
 
+  // Task 11.2: do Task 11.1 with vector iterator
 
-  // Task 12: Create a ParticleContainer to store particles. Each particle store x,y,z,vx,vy,vz,w and ID, where w is the weight or 'mass' of a particle and it is a real number. Loop through all particles and set initial conditions. 
-int n = 64;
-Box domain({1,1,1},{n,n,n});
-RealBox real_box({0.0,0.0,0.0},{1.0,1.0,1.0});
-Geometry geom(domain, real_box, 0, {1,1,1});
-BoxArray ba(domain);
-ba.maxSize(8);
+  // Task 11.3: We do not directly use std::vector too much, but it can help us
+  // to get familar with c++ containers. The best way to lean vector is to
+  // design and implement an simple vector template. Here is a simple
+  // implementation:https://www.geeksforgeeks.org/how-to-implement-our-own-vector-class-in-c/
+  // Think about the questions: 1. What is the difference between
+  // 'size' and 'capacity'? 2. What will happen if a vector is already 'full',
+  // but push() is still called to add more more elements?
 
+  // Task 11.4: Implement you own vector class MyClass. See instructions in
+  // MyVector.h
 
-DistributionMapping dm {ba};
+  // Task 12: Create a ParticleContainer to store particles. Each particle store
+  // x,y,z,vx,vy,vz,w and ID, where w is the weight or 'mass' of a particle and
+  // it is a real number. Loop through all particles and set initial conditions.
+  int n = 64;
+  Box domain({ 1, 1, 1 }, { n, n, n });
+  RealBox real_box({ 0.0, 0.0, 0.0 }, { 1.0, 1.0, 1.0 });
+  Geometry geom(domain, real_box, 0, { 1, 1, 1 });
+  BoxArray ba(domain);
+  ba.maxSize(8);
 
+  DistributionMapping dm{ ba };
 
-int lev =0;
+  int lev = 0;
 
+  ParticleContainer<1, 0> PC(geom, dm, ba);
 
-ParticleContainer<1,0> PC(geom,dm,ba);
+  auto& particle_tile = PC.GetParticles(lev)[std::make_pair(1, 1)];
 
+  for (int ii = 0; ii < 10; ++ii)
 
-
-auto& particle_tile = PC.GetParticles(lev)[std::make_pair(1,1)];
-
-for (int ii=0; ii<10; ++ii) 
-
-{
-    Particle<1,0> p;
-    p.id()   = ii;
-    p.cpu()  = ParallelDescriptor::MyProc();
-    p.pos(0) = 0.05*ii;
-    p.pos(1) = 0.05*ii;
-    p.pos(2) = 0.05*ii;
+  {
+    Particle<1, 0> p;
+    p.id() = ii;
+    p.cpu() = ParallelDescriptor::MyProc();
+    p.pos(0) = 0.05 * ii;
+    p.pos(1) = 0.05 * ii;
+    p.pos(2) = 0.05 * ii;
     particle_tile.push_back(p);
+  }
 
-}
+  // Print()<<particle_tile[1];
 
-//Print()<<particle_tile[1];
+  // Print()<<
 
-
-
-
-//Print()<<
-
-
-
-  // Task 13: update particle locations with the corresponding velocities. 1st order accuracy is good enough. 
-
+  // Task 13: update particle locations with the corresponding velocities. 1st
+  // order accuracy is good enough.
 
   amrex::Finalize();
 }
